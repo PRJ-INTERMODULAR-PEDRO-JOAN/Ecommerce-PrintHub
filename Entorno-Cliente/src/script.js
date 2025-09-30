511596ad1dfc0b6f62a91fe99c4813b027bf13f9
Pedro <pedrocorreo@gmail.com>
2025-09-30 16:51:29 +0200

    Mejorado el index en entorno Cliente

--- Entorno-Cliente/src/script.js
+++ Entorno-Cliente/src/script.js
@@ -1,7 +1,15 @@
- // Script per a interacció bàsica, com per exemple mostrar un missatge quan s'hi fa clic en el botó de veure més
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Estàs veient més informació del producte!');
-    });
+// Selección de elementos
+const sidebar = document.querySelector(".sidebar");
+const toggleBtn = document.querySelector(".menu-toggle");
+
+// Toggle del menú lateral (abrir/cerrar con el mismo botón)
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
 });
 
+// Missatge botons productes
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Estàs veient més informació del producte!');
+  });
+});
