ff21afc5c8de21fc58ee75b2faba9d839a2bf0e9
Pedro <pedrocorreo@gmail.com>
2025-11-14 16:12:57 +0100

    Mejorado

--- PrintHub/php/contact.php
+++ PrintHub/php/contact.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <div class="barra-lateral-cabecera">
       <h1 class="logo-texto">Print<span class="resaltado">Hub</span></h1>
       <div class="logo">
-        <img src="public/logoPrintHub.jpeg" alt="Logo de PrintHub" />
+        <img src="../public/logoPrintHub.jpeg" alt="Logo de PrintHub" />
       </div>
     </div>
 
@@ -121,44 +121,48 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     </section>
   </main>
 
-  <footer class="footer">
-    <div class="footer-container">
-      <div class="footer-left">
-        <h2 class="logo">Print<span class="highlight">Hub</span></h2><br>
-        <div>
-          <a class="social-icons" href="#"><img src="/public/facebook.jpg" alt="Facebook"></a>
-          <a class="social-icons" href="#"><img src="/public/linkedIn.png" alt="LinkedIn"></a>
-          <a class="youtube-icons" href="#"><img src="/public/youtube.png" alt="YouTube"></a>
-          <a class="social-icons" href="#"><img src="/public/insta.jpg" alt="Instagram"></a>
+  <footer class="pie-pagina">
+      <div class="pie-pagina-contenedor">
+        
+        <div class="pie-pagina-izquierda">
+          <h2 class="logo">
+            Print<span class="resaltado">Hub</span>
+          </h2>
+          <br>
+          <div>
+            <a class="iconos-sociales" href="#"><img src="../public/facebook.jpg" alt="Facebook"></a>
+            <a class="iconos-sociales" href="#"><img src="../public/linkedIn.png" alt="LinkedIn"></a>
+            <a class="iconos-youtube" href="#"><img src="../public/youtube.png" alt="YouTube"></a>
+            <a class="iconos-sociales" href="#"><img src="../public/insta.jpg" alt="Instagram"></a>
+          </div>
+        </div>
+  
+        <div class="pie-pagina-centro">
+          <img src="../public/fotterImage.jpg" alt="Imagen global 3D">
+        </div>
+  
+        <div class="pie-pagina-derecha">
+          <div class="servicios">
+            <h3>Servicios y productos</h3>
+            <ul>
+              <li><a href="#">Fabricación aditiva 3D</a></li>
+              <li><a href="#">Servicio de Impresión 3D</a></li>
+              <li><a href="#">Prototipado 3D</a></li>
+              <li><a href="#">Ingeniería y diseño</a></li>
+            </ul>
+          </div>
+          <div class="contacto">
+            <h3>Contacto</h3>
+            <ul>
+              <li>📞 (+34) 951 753 852</li>
+              <li>📧 <a href="mailto:printhub@contact.me">printhub@contact.me</a></li>
+              <li>📍 Partida Cotes Altes, 27, 03804 Alcoi, Alicante</li>
+              <li>🕒 Lunes – Viernes: 8:00 – 13:30</li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div class="footer-center">
-        <img src="/public/fotterImage.png" alt="Imagen global 3D">
-      </div>
-
-      <div class="footer-right">
-        <div class="services">
-          <h3>Servicios y productos</h3>
-          <ul>
-            <li><a href="#">Fabricación aditiva 3D</a></li>
-            <li><a href="#">Servicio de Impresión 3D</a></li>
-            <li><a href="#">Prototipado 3D</a></li>
-            <li><a href="#">Ingeniería y diseño</a></li>
-          </ul>
-        </div>
-        <div class="contact">
-          <h3>Contacto</h3>
-          <ul>
-            <li>📞 (+34) 951 753 852</li>
-            <li>📧 <a href="mailto:printhub@contact.me">printhub@contact.me</a></li>
-            <li>📍 Partida Cotes Altes, 27, 03804 Alcoi, Alicante</li>
-            <li>🕒 Lunes – Viernes: 8:00 – 13:30</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
+    </footer>
 
   <script>
   // Selección de elementos
